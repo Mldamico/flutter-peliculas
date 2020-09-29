0c43f9048893560dc5630e365745aac56933daee
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-29 14:56:18 -0300

    Basic stack card

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Probando')),
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Peliculas'),
+          backgroundColor: Colors.indigoAccent,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              _swiperTarjetas(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        itemWidth: 200.0,
+        layout: SwiperLayout.STACK,
+        // pagination: new SwiperPagination(),
+        // control: new SwiperControl(),
+      ),
     );
   }
 }
